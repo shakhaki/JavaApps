999c6023dcd1f26a95222b7ae9862e72f046688a
Justin <stockdile@yahoo.com>
2016-11-14 22:26:37 -0700

    Chapter 6

---
+++ CollegeCost/src/collegecost/CollegeCost.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * Justin Lee
+ * IS 4415
+ */
+package collegecost;
+
+/**
+ *
+ * @author Admin
+ */
+public class CollegeCost {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
